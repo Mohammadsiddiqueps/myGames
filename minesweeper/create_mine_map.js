33058654a87b3a0b91f5c8b7bc51611335c42926
Mohammadsiddiqueps <126324832+Mohammadsiddiqueps@users.noreply.github.com>
2025-01-19 17:36:14 +0530

    moved the code for creating minemap to here

---
+++ minesweeper/create_mine_map.js
@@ -0,0 +1,94 @@
+// import { CELL_COUNT, NO_OF_BOMBS } from "./mine_sweeper.js";
+export const NO_OF_BOMBS = 15;
+export const CELL_COUNT = 100;
+const getBombPositions = (noOfBombs, bombPositions) => {
+  if (bombPositions.length === noOfBombs) {
+    return bombPositions;
+  }
+
+  const randomNumber = Math.floor(Math.random() * 100);
+
+  if (bombPositions.includes(randomNumber)) {
+    return getBombPositions(noOfBombs, bombPositions);
+  }
+
+  return getBombPositions(noOfBombs, [...bombPositions, randomNumber]);
+};
+
+function createGround(limit, bombPositions) {
+  const ground = Array(limit).fill(0);
+  bombPositions.map((x) => (ground[x] = "B"));
+
+  return ground;
+}
+
+const getPositionInfo = (index) => {
+  return [
+    index - 10 < 0,
+    index + 10 > 99,
+    index % 10 === 0,
+    (index + 1) % 10 === 0,
+  ];
+};
+
+const getIncrementConditions = (index) => {
+  const [isAtFirstRow, isAtLastRow, isAtLeftRow, isAtRightRow] =
+    getPositionInfo(index);
+  return [
+    !isAtFirstRow && !isAtLeftRow,
+    !isAtFirstRow && !isAtRightRow,
+    !isAtLastRow && !isAtLeftRow,
+    !isAtLastRow && !isAtRightRow,
+    !isAtLeftRow,
+    !isAtFirstRow,
+    !isAtRightRow,
+    !isAtLastRow,
+  ];
+};
+
+function getPosToIncrement(index) {
+  const arrayCondition = getIncrementConditions(index);
+
+  const indexes = [
+    index - 11,
+    index - 9,
+    index + 9,
+    index + 11,
+    index - 1,
+    index - 10,
+    index + 1,
+    index + 10,
+  ];
+
+  return arrayCondition.reduce((a, b, i) => {
+    if (b) {
+      return [...a, indexes[i]];
+    }
+    return [...a];
+  }, []);
+}
+
+function getIncrementedString(mineMap, posToIncrement) {
+  posToIncrement.map((x) => {
+    if (mineMap[x] !== "B") {
+      mineMap[x]++;
+    }
+  });
+  return mineMap;
+}
+
+function setMineCount(mineMap) {
+  for (let index = 0; index < mineMap.length; index++) {
+    if (mineMap[index] === "B") {
+      const posToIncrement = getPosToIncrement(index);
+      mineMap = getIncrementedString(mineMap, posToIncrement);
+    }
+  }
+
+  return mineMap;
+}
+console.log(NO_OF_BOMBS, CELL_COUNT);
+
+export const bombPositions = getBombPositions(NO_OF_BOMBS, []);
+ const mineGround = createGround(CELL_COUNT, bombPositions);
+export const mineMap = setMineCount(mineGround);
