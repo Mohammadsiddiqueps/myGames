f16d267bc568ffb6caebcaaca4a8041dd79d2eb2
Mohammadsiddiqueps <126324832+Mohammadsiddiqueps@users.noreply.github.com>
2025-03-23 19:41:01 +0530

    created the game loop structure and the turns

--- connectFour/client.js
+++ connectFour/client.js
@@ -1,8 +1,67 @@
-const connection = await Deno.connect({ port: 8000 });
+import { getUserInput } from "./connect_four.js";
 
-const name = prompt("Enter Your Name: ");
+class ConnectFourClient {
+  async init(port) {
+    this.connection = await Deno.connect({ port });
 
-console.log("this is the name user gave: ", name);
-await connection.write(new TextEncoder().encode(name));
+    await this.sendName();
+    await this.gameIntro();
+    await this.gameLoop();
+  }
 
-console.log("name has send");
+  async sendName() {
+    this.name = prompt("Enter Your Name: ");
+    console.log("This is the name user gave:", this.name);
+    await this.connection.write(new TextEncoder().encode(this.name));
+    console.log("Name has been sent.");
+  }
+
+  async gameIntro() {
+    console.log("Waiting for the opponent...");
+
+    const startMsg = await this.readMessage();
+    console.log(startMsg);
+  }
+
+  async gameLoop() {
+    while (true) {
+      console.log("Waiting for the server message...");
+
+      const serverMessage = await this.readMessage();
+      console.log("Server message:", serverMessage);
+
+      if (serverMessage === "WAIT") {
+        await this.handleOpponentMove();
+        continue;
+      }
+
+      await this.handleTurn();
+    }
+  }
+
+  async handleOpponentMove() {
+    console.log("Opponent is playing...⏳");
+    const move = await this.readMessage();
+    console.log("Opponent's move:", move);
+  }
+
+  async handleTurn() {
+    console.log("Your turn! 🎮");
+    const playerInput = prompt("Enter Column No to Drop: ");
+    console.log("This is the input of the player:", playerInput);
+    await this.connection.write(new TextEncoder().encode(playerInput));
+
+    const move = await this.readMessage();
+    console.log("My move:", move);
+  }
+
+  async readMessage() {
+    const buff = new Uint8Array(1024);
+    const byteCount = await this.connection.read(buff);
+    // if (!byteCount) return "";
+    return new TextDecoder().decode(buff.slice(0, byteCount)).trim();
+  }
+}
+
+const client = new ConnectFourClient();
+await client.init(8000);
